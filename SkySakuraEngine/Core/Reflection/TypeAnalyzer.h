869188ad1aa4db7e18d014b1a5c9353f05b19590
SkySakura2005 <wuxinyang89@gmail.com>
2026-07-22 00:17:38 +0800

    新增类型分析代码

---
+++ SkySakuraEngine/Core/Reflection/TypeAnalyzer.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+template <typename T, typename U>
+struct is_same_type;
+
+template <typename T, typename U>
+struct is_same_type<T, U> {
+    static const bool value = false;
+};
+
+template <typename T>
+struct is_same_type<T, T> {
+    static const bool value = true;
+};
+
+template <typename T>
+struct is_pointer {
+    static const bool value = false;
+};
+
+template <typename T>
+struct is_pointer<T*> {
+    static const bool value = true;
+};
